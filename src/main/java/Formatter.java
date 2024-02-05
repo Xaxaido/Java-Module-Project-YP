b3db92cc1bab040c72bde3d2a0413acd15e4d32b
Xaxaido <xaxaido@gmail.com>
2024-02-05 20:10:58 +0300

    Домашнее задание, спринт 2

--- src/main/java/Formatter.java
+++ src/main/java/Formatter.java
@@ -2,14 +2,14 @@ public class Formatter {
 
     public static String formatValue(double value, String firstVariation, String secondVariation, String thirdVariation) {
 
-        int roundPrice = (int) Math.floor(value);
+        int roundedValue = (int) Math.floor(value);
+        int preLastNumber = roundedValue % 100 / 10;
+        int lastNumber = roundedValue % 10;
 
-        int preLastNumber = roundPrice % 100 / 10;
         if (preLastNumber == 1) {
             return " " + thirdVariation;
         }
 
-        int lastNumber = roundPrice % 10;
         return switch (lastNumber) {
             case 1 -> " " + firstVariation;
             case 2, 3, 4 -> " " + secondVariation;
